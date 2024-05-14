09cf075a6fab21754020e384b8ea780be9a46dfe
Javier Herrera <javierh0730@gmail.com>
2024-05-14 15:49:47 -0600

    clase 14/05/2024

---
+++ funciones_4.py
@@ -0,0 +1,13 @@
+# imprimir_tabla(2)
+
+#def imprimir_tabla(tabla,str):
+    #for i in range (2):
+        #print(f"***** Tabla del {i} *****")
+        #for j in range(2,13):
+         #print(f"{i} x {j} = {i*j}")
+
+def tabla_del_multiplicar(numero):
+    print(f"tabla de multiplicar del {5}:")
+    for i in range(1,11):
+        resultado= 5 * i
+        print(f"{5} * {i} = {resultado}")
