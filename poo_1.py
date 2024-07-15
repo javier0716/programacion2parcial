791bd11887d91594051fda9c3591dd32db06a019
Javier Herrera <javierh0730@gmail.com>
2024-07-15 15:08:26 -0600

    15/07/2024

--- poo_1.py
+++ poo_1.py
@@ -1,20 +1,32 @@
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 
 # init se llama constructor
 # los nombres de clases siempre empiezan con MAYUSCULA
 class Todo: 
     def __init__(self, nombre: str, completada : bool = False):
-        self.__id__ = uuid4()
-        self.nombre = nombre
+        self.__id = uuid4()
+        self.__nombre = nombre
         self.completada = completada
-        self.hora = "16:18"
+        
+    def get_id(self) -> UUID:
+        return self.__id
+    
+    def get_mombre(self) -> str:
+        return self.__nombre
+    
+    def set_nombre(self, nombre: str):
+        self.__nombre = nombre   
+    
 todo_1 = Todo("Test 1")
 todo_2 = Todo("Test 2")
 
 
-print(todo_1.__id__)
+print(todo_1.get_id())
+print(todo_1.get_mombre())
+todo_1.set_nombre("Test 3")
+print(todo_1.get_mombre())
 
-todo_1.id = "ABC123"
+# todo_1.__id = "ABC123"
 
-print(todo_1.__id__)
+# zprint(todo_1.__id)      
