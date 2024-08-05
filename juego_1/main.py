6672bdb9e16d3f0e26bbcf16b25bfb7f079a5095
Javier Herrera <javierh0730@gmail.com>
2024-08-05 13:59:37 -0600

    05/08/2024

--- juego_1/main.py
+++ juego_1/main.py
@@ -8,10 +8,29 @@ screen = pygame.display.set_mode((800,600))
 running = True 
 
 
+coord_x = 390
+coord_y = 290
+
+speed_x = 1
+speed_y = 1
 while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+            
+    coord_x += speed_x
     
+    
+    if coord_x >= 780 or coord_x <= 0:
+       speed_x *= -1
+    
+    coord_y += speed_y
+    
+    if coord_y>= 580 or coord_y<= 0:
+        speed_y *= -1
+
     screen.fill("black")
-    
+    pygame.draw.rect(screen, "white", (coord_x, coord_y, 20, 20))
     pygame.display.flip()
 
 pygame.quit()
