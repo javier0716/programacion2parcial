128542725b253950367b34799da181b2bcb8d50b
Javier Herrera <javierh0730@gmail.com>
2024-08-08 15:09:08 -0600

    8/8/2024

--- juego_2/main.py
+++ juego_2/main.py
@@ -1,6 +1,5 @@
 import pygame 
 
-
 pygame.init()
 
 screen = pygame.display.set_mode((800,600), flags=pygame.SCALED, vsync=1)
@@ -10,8 +9,12 @@ running = True
 bg_color = [0,0,0]
 
 clock = pygame.time.Clock()
+
+PLAYER_MAX_SPEED = 200
+
 player_1_coord_x = 40
 player_1_coord_y = 225
+player_1_speed = 0
 
 while running: 
     for event in pygame.event.get():
@@ -20,14 +23,22 @@ while running:
             
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                print("w")
+             player_1_speed = PLAYER_MAX_SPEED * -1
+             if event.key == pygame.K_s:
+                 player_1_speed = PLAYER_MAX_SPEED
         
         elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                player_1_speed = 0
             if event.key == pygame.K_s:
-                print("s")
+                player_1_speed = PLAYER_MAX_SPEED
+                
+        
         
 
     delta_time = clock.tick(60) / 1000
+    
+    player_1_coord_y += player_1_speed * delta_time
 
     screen.fill(bg_color)
     
