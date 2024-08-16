a6338e042984f2e85f56eb13bc2734e906fa13b4
Javier Herrera <javierh0730@gmail.com>
2024-08-15 18:07:59 -0600

    15/05/2024

--- juego_2/main.py
+++ juego_2/main.py
@@ -9,12 +9,21 @@ running = True
 bg_color = [0,0,0]
 
 clock = pygame.time.Clock()
-
 PLAYER_MAX_SPEED = 400
+
+ball_coord_x = 390
+ball_coord_y = 290
+
+ball_speed_x = 225
+ball_speed_y = 225
+
 
 player_1_coord_x = 40
 player_1_coord_y = 225
 player_1_speed = 0
+
+player_2_coord_x = 740
+
 
 while running: 
     for event in pygame.event.get():
@@ -26,6 +35,7 @@ while running:
              player_1_speed = PLAYER_MAX_SPEED * -1
             if event.key == pygame.K_s:
                 player_1_speed = PLAYER_MAX_SPEED
+                
         
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_w:
@@ -40,15 +50,26 @@ while running:
     
     player_1_coord_y += player_1_speed * delta_time
     
+    
+    if ball_coord_y > 580:
+        ball_coord_y = 580
+        
+    elif ball_coord_y < 0:
+        ball_coord_y = 0
+        
     if player_1_coord_y < 0:
         player_1_coord_y = 0
         
     if player_1_coord_y > 450:
         player_1_coord_y = 450
+        
+    ball_coord_y += ball_speed_y * delta_time
 
+    
     screen.fill(bg_color)
     
     pygame.draw.rect(screen,"white", (player_1_coord_x, player_1_coord_y, 25, 150))
+    pygame.draw.rect(screen,"white",(ball_coord_x, ball_coord_y, 20, 20))
     
     
     pygame.display.flip()
