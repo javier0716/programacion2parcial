0ba79258df277642e92f95a896817d911dd0fc05
javier0716 <javierh0730@gmail.com>
2024-10-07 14:43:57 -0600

    7/10/2024

--- juego_2/main.py
+++ juego_2/main.py
@@ -1,76 +1,83 @@
 import pygame 
-
+from player import Player
+from config import Config
 pygame.init()
 
-screen = pygame.display.set_mode((800,600), flags=pygame.SCALED, vsync=1)
+screen = pygame.display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT), flags=pygame.SCALED, vsync=1)
 
-running = True 
+running = True
+
+pelota_coord_x = 390
+pelota_coord_y = 290
+
+pelota_speed_x = 200
+pelota_speed_y = 200
 
 bg_color = [0,0,0]
 
-clock = pygame.time.Clock()
-PLAYER_MAX_SPEED = 400
+clock =pygame.time.Clock()
 
-ball_coord_x = 390
-ball_coord_y = 290
+player_1 = Player (
+    screen,
+      Config.PLAYER_BORDERDISTANCE,
+        (Config.SCREEN_WIDTH/2) - (Config.PLAYER_HEIGHT/2),
+        Config.PLAYER_WIDTH,
+        Config.PLAYER_HEIGHT
+      )
+player_2 = Player (
+    screen,
+      Config.SCREEN_WIDTH - Config.PLAYER_BORDERDISTANCE - Config.PLAYER_WIDTH,
+        (Config.SCREEN_WIDTH/2) - (Config.PLAYER_HEIGHT/2),
+        Config.PLAYER_WIDTH,
+        Config.PLAYER_HEIGHT
+        )
 
-ball_speed_x = 225
-ball_speed_y = 225
 
-
-player_1_coord_x = 40
-player_1_coord_y = 225
-player_1_speed = 0
-
-player_2_coord_x = 740
-
-
-while running: 
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
+            running = False
             
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-             player_1_speed = PLAYER_MAX_SPEED * -1
+                player_1.set_velocidad(Config.PLAYER_MAX_SPEED * -1)
             if event.key == pygame.K_s:
-                player_1_speed = PLAYER_MAX_SPEED
-                
+                player_1.set_velocidad(Config.PLAYER_MAX_SPEED)
+            if event.key == pygame.K_UP:
+                player_2.set_velocidad(Config.PLAYER_MAX_SPEED * -1)
+            if event.key == pygame.K_DOWN:
+                    player_2.set_velocidad(Config.PLAYER_MAX_SPEED)
         
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_w:
-                player_1_speed = 0
-            if event.key == pygame.K_s:
-                player_1_speed = 0
-                
-        
-        
+            if event.key == pygame.K_w or event.key == pygame.K_s:
+                player_1.set_velocidad(0)
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                player_2.set_velocidad(0)
 
     delta_time = clock.tick(60) / 1000
-    
-    player_1_coord_y += player_1_speed * delta_time
-    
-    
-    if ball_coord_y > 580:
-        ball_coord_y = 580
-        
-    elif ball_coord_y < 0:
-        ball_coord_y = 0
-        
-    if player_1_coord_y < 0:
-        player_1_coord_y = 0
-        
-    if player_1_coord_y > 450:
-        player_1_coord_y = 450
-        
-    ball_coord_y += ball_speed_y * delta_time
+    player_1.move(delta_time)
+    player_2.move(delta_time)
 
+    pelota_coord_y += pelota_speed_y * delta_time
+
+    if pelota_coord_y > 580:
+        pelota_coord_y = 580
     
+    elif pelota_coord_y < 0:
+        pelota_coord_y = 0
+
+    if pelota_coord_y >= 580 or pelota_coord_y <= 0:
+        pelota_speed_y *= -1
+
+    pelota_coord_x += pelota_speed_x * delta_time
+
     screen.fill(bg_color)
     
-    pygame.draw.rect(screen,"white", (player_1_coord_x, player_1_coord_y, 25, 150))
-    pygame.draw.rect(screen,"white",(ball_coord_x, ball_coord_y, 20, 20))
-    
+    #pygame.draw.rect(screen, "white", (player_1_coord_x, player_1_coord_y, 25, 150))
+    player_1.draw()
+    player_2.draw()
+    pygame.draw.rect(screen, "white",(pelota_coord_x, pelota_coord_y, 20, 20))
+    pygame.display.flip()
     
     pygame.display.flip()
 
---
+++ juego_2/player.py
@@ -0,0 +1,32 @@
+from pygame import surface
+import pygame
+
+
+
+class Player:
+    def __init__(self, screen:surface ,x:int ,y:int ,ancho:int = 25, alto:int = 150) -> None:
+        self.__x = x
+        self.__y = y 
+        self.__ancho = ancho
+        self.__alto = alto
+        self.__velocidad = 0
+        self.__screen = screen
+
+    def set_velocidad(self, velocidad: int):
+        self.__velocidad = velocidad
+
+    def draw(self) -> None:
+        pygame.draw.rect(
+            self.__screen,
+            "white",
+            (self.__x, self.__y, self.__ancho, self.__alto)
+        )          
+
+    def move(self, delta_time: float) -> None:
+        self.__y += self.__velocidad * delta_time
+
+        if self.__y < 0:
+            self.__y = 0
+
+        if self.__y > 450:
+            self.__y = 450  
