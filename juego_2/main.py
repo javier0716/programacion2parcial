a8711329507cd01bff6f89066c9cdfcac12922a7
javier0716 <javierh0730@gmail.com>
2024-10-07 15:09:31 -0600

    07/10/2024

--- juego_2/main.py
+++ juego_2/main.py
@@ -7,30 +7,23 @@ screen = pygame.display.set_mode((Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT), fl
 
 running = True
 
-pelota_coord_x = 390
-pelota_coord_y = 290
-
-pelota_speed_x = 200
-pelota_speed_y = 200
-
 bg_color = [0,0,0]
 
 clock =pygame.time.Clock()
-
 player_1 = Player (
     screen,
-      Config.PLAYER_BORDERDISTANCE,
+    20,
         (Config.SCREEN_WIDTH/2) - (Config.PLAYER_HEIGHT/2),
         Config.PLAYER_WIDTH,
         Config.PLAYER_HEIGHT
-      )
+)
 player_2 = Player (
     screen,
-      Config.SCREEN_WIDTH - Config.PLAYER_BORDERDISTANCE - Config.PLAYER_WIDTH,
+      Config.SCREEN_WIDTH - 20  - Config.PLAYER_WIDTH,
         (Config.SCREEN_WIDTH/2) - (Config.PLAYER_HEIGHT/2),
         Config.PLAYER_WIDTH,
         Config.PLAYER_HEIGHT
-        )
+)
 
 
 while running:
@@ -54,29 +47,13 @@ while running:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 player_2.set_velocidad(0)
 
-    delta_time = clock.tick(60) / 1000
+    delta_time = clock.tick(120) / 1000
     player_1.move(delta_time)
     player_2.move(delta_time)
 
-    pelota_coord_y += pelota_speed_y * delta_time
-
-    if pelota_coord_y > 580:
-        pelota_coord_y = 580
-    
-    elif pelota_coord_y < 0:
-        pelota_coord_y = 0
-
-    if pelota_coord_y >= 580 or pelota_coord_y <= 0:
-        pelota_speed_y *= -1
-
-    pelota_coord_x += pelota_speed_x * delta_time
-
     screen.fill(bg_color)
-    
-    #pygame.draw.rect(screen, "white", (player_1_coord_x, player_1_coord_y, 25, 150))
     player_1.draw()
     player_2.draw()
-    pygame.draw.rect(screen, "white",(pelota_coord_x, pelota_coord_y, 20, 20))
     pygame.display.flip()
     
     pygame.display.flip()
